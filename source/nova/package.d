b901dd40f5e0d381f6092d10b6c75381d975c2da
Navid M <navidmomtahen@gmail.com>
2026-01-14 00:02:32 +0000

    fmt

--- source/nova/package.d
+++ source/nova/package.d
@@ -146,7 +146,6 @@ unittest
 
     Vec2 screenPos = Vec2(960, 540);
     Vec2 expectedWorld = Vec2(0, 0);
-
     Vec2 worldPos = Vec2((screenPos.x / 1920.0f - 0.5f) * 2.0f,
             -(screenPos.y / 1080.0f - 0.5f) * 2.0f);
 
